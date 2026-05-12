479b0a819538f4f2ba1a83f43c02cf24c6ae0547
barack1212 <barackkwise1122@gmail.com>
2026-05-12 13:36:37 +0200

    new update

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-const server = require("http")
+const server = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log("success")
+})
+
+server.listen(3000, () => {
+    console.log("server is running on port 3000")
+})
